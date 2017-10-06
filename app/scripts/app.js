c0af81b6f202df4d37c859ea6dd4e8ba96e07596
aaronbulnes <aaronchrisbulnes@gmail.com>
2017-10-06 10:35:34 -0500

    fixed merge conflicts

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,80 +1,33 @@
 
 (function() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-     function config($stateProvider, $locationProvider) {
 
-  $locationProvider
-         .html5Mode({
-             enabled: true,
-             requireBase: false
-         });
-
-$stateProvider
-         .state('landing', {
-             url: '/',
-             controller: 'LandingCtrl as landing',
-             controller: 'Album as album'
-             controller: 'CollectionCtrl as collection',
-             templateUrl: '/templates/landing.html'
-
-     }
- 
-     angular
-         .module('blocJams', ['ui.router'])
-         .config(config);
- })();
-=======
-=======
->>>>>>> checkpoint-4
-=======
->>>>>>> checkpoint-5
     function config($stateProvider, $locationProvider) {
         $locationProvider
             .html5Mode({
                 enabled: true,
                 requireBase: false
-            })
+            });
         $stateProvider
             .state('landing', {
                 url: '/',
-<<<<<<< HEAD
-=======
                 controller: 'LandingCtrl as landing',
->>>>>>> checkpoint-5
                 templateUrl: '/templates/landing.html'
             })
             .state('album', {
                 url: '/album',
+                controller: 'AlbumCtrl as album',
                 templateUrl: '/templates/album.html'
-<<<<<<< HEAD
-<<<<<<< HEAD
-            })
-            .state('collection', {
-                url: '/collection',
-                templateUrl: '/templates/collection.html'
-=======
->>>>>>> checkpoint-4
-=======
+            
             })
             .state('collection', {
                 url: '/collection',
                 controller: 'CollectionCtrl as collection',
                 templateUrl: '/templates/collection.html'
->>>>>>> checkpoint-5
             });
     }
     
     angular
         .module('blocJams', ['ui.router'])
         .config(config);
-<<<<<<< HEAD
+
 })();
-<<<<<<< HEAD
->>>>>>> checkpoint-3
-=======
->>>>>>> checkpoint-4
-=======
-})();
->>>>>>> checkpoint-5
