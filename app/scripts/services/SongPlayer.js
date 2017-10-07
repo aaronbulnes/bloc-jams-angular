13f57a11dbf11197ada282240fd77d21984a7fe8
aaronbulnes <aaronchrisbulnes@gmail.com>
2017-10-07 15:53:39 -0500

    checkpoint 7 finished

---
+++ app/scripts/services/SongPlayer.js
@@ -0,0 +1,28 @@
+(function() {
+    function SongPlayer() {
+         var SongPlayer = {};
+
+         var currentSong = null;
+         var currentBuzzObject = null;
+
+         SongPlayer.play = function(song) {
+            currentBuzzObject = new buzz.sound(song.audioUrl, {
+                formats: ['mp3'],
+                preload: true
+            });
+
+            currentSong = song;
+    
+            currentBuzzObject.play();    
+
+            }
+
+        };
+
+         return SongPlayer;
+    }
+
+    angular
+        .module('blocJams')
+        .factory('SongPlayer', SongPlayer);
+})();
