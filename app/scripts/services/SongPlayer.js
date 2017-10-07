dfb5115b465caa629eb442bfad0143d2f5e5ec9e
aaronbulnes <aaronchrisbulnes@gmail.com>
2017-10-07 16:18:43 -0500

    checkpoint 7 assignment

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -1,27 +1,73 @@
 (function() {
     function SongPlayer() {
-         var SongPlayer = {};
-
-         var currentSong = null;
-         var currentBuzzObject = null;
-
-         SongPlayer.play = function(song) {
+        var SongPlayer = {};
+        
+//        @desc current song object set to null when page loads
+//        @type {Object}
+        var currentSong = null;
+        
+//        @desc Buzz object audio file
+//        @type {Object}
+        var currentBuzzObject = null;
+        
+//        @function setSong
+//        @desc Stops currently playing song and loads new audio file as currentBuzzObject
+//        @param {Object} song
+        var setSong = function(song) {
+            if (currentBuzzObject) {
+                currentBuzzObject.stop();
+                currentSong.playing = null;
+            }
+            
             currentBuzzObject = new buzz.sound(song.audioUrl, {
                 formats: ['mp3'],
                 preload: true
             });
-
+            
             currentSong = song;
-    
-            currentBuzzObject.play();    
-
-            }
-
         };
-
-         return SongPlayer;
+        
+//        @function playSong
+//        @desc it plays song and lists song.playing to true so album.html changes to the proper play button
+//        @param {Object} song
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        }
+        
+//        @function pauseSong
+//        @desc this pauses song and sets song.playing to false so album.html changes to the pause icon
+//        @param {Object} song
+        var pauseSong = function(song) {
+            currentBuzzObject.pause();
+            song.playing = false;
+        }
+        
+//        @function SongPlayer.play(song)
+//        @desc plays a song from the beginning if the song has been played yet and then resumes playing from where it was left off
+//        @params {Object} song
+        SongPlayer.play = function(song) {
+            if (currentSong !== song) {
+                setSong(song);
+                playSong(song);
+                
+            } else if (currentSong === song) {
+                if (currentBuzzObject.isPaused()) {
+                    playSong(song);
+                }
+            }
+        };
+        
+//        @function SongPlayer.pause(song)
+//        @desc thuis pauses a song at its current time stamp
+//        @params {Object} song
+        SongPlayer.pause = function(song) {
+            pauseSong(song);
+        };
+        
+        return SongPlayer;
     }
-
+    
     angular
         .module('blocJams')
         .factory('SongPlayer', SongPlayer);
