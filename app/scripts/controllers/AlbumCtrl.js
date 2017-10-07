2e4b2e24ebe48332987a99f58d56943a3339d270
aaronbulnes <aaronchrisbulnes@gmail.com>
2017-10-07 15:06:02 -0500

    fixed chapter 6

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,9 +1,8 @@
-( function() {
-     function AlbumCtrl(Fixtures) {
-         this.albumData = Fixtures.getAlbum();
-     }
-     
-     angular
-         .module('blocJams')
-         .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
- })(); 
+(function() {
+    function AlbumCtrl(Fixtures) {
+        this.albumData = Fixtures.getAlbum();
+    
+    angular
+        .module('blocJams')
+        .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+})();
